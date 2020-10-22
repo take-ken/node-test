35c75301bcde75d3f949e1dd3d71b96675cec322
take-ken <take0516ball@gmail.com>
2020-10-23 05:33:18 +0900

    OAuth認証

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ var path = require('path');
 var http = require('http');
 var express = require('express');
 var mongoose = require('mongoose');
-var Message = require('./schema/Message');
 var bodyParser = require('body-parser');
 var fileUpload = require('express-fileupload');
+var passport = require('passport');
+var session = require('express-session');
+
+var TwitterStrategy = require('passport-twitter').Strategy;
+
+var Message = require('./schema/Message');
+var User = require('./schema/User');
+const { pathToFileURL } = require('url');
+
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/chatapp', function (err) {
@@ -18,18 +26,73 @@ mongoose.connect('mongodb://localhost:27017/chatapp', function (err) {
 });
 
 app.use(bodyParser())
+app.use(session({ secret: 'HogeFuga' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views',
     path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use("/image", express.static(path.join(__dirname, 'image')));
+app.use("/avatar", express.static(path.join(__dirname, 'avatar')));
 
 app.get("/", function (req, res, next) {
     Message.find({}, function (err, msgs) {
         if (err) throw err;
-        return res.render('index',
-            { messages: msgs });
+        return res.render('index', {
+            messages: msgs,
+            user: req.session && req.session.user ? req.session.user : null
+        });
+    });
+});
+
+
+
+passport.use(new TwitterStrategy(twitterConfig, function (token, tokenSecret, profile, done) {
+    User.findOne({ twitter_profile_id: profile.id }, function (err, user) {
+        if (err) {
+            return done(err);
+        } else if (!user) {
+            var _user = {
+                username: profile.displayName,
+                twitter_profile_id: profile.id,
+                avatar_path: profile.photos[0].value
+            };
+            var newUser = new User(_user);
+            newUser.save(function (err) {
+                if (err) throw err
+                return done(null, newUser);
+            });
+        } else {
+            return done(null, user);
+        }
+    });
+}
+));
+
+app.get('/oauth/twitter', passport.authenticate('twitter'));
+
+app.get('/oauth/twitter/callback', pasport.authenticate('twitter'), function (req, res, next) {
+    User.findOne({ _id: req.session.passport.user }, function (err, user) {
+        if (err || !req.session)
+            return res.redirect('/oauth/twitter')
+        req.session.user = {
+            username: user.username,
+            avatar_path: user.avatar_path
+        }
+        return res.redirect("/")
+    })
+}
+);
+
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findOne({ _id: id }, function (err, user) {
+        done(err, user);
     });
 });
 
